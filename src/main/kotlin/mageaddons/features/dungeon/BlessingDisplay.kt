5dd8de8e090bdca2c9518f9d40ac8cda56d222cd
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2025-03-16 12:30:59 +0800

    blessing packet read

--- src/main/kotlin/mageaddons/features/dungeon/BlessingDisplay.kt
+++ src/main/kotlin/mageaddons/features/dungeon/BlessingDisplay.kt
@@ -2,14 +2,21 @@ package mageaddons.features.dungeon
 
 import mageaddons.config.Config
 import mageaddons.core.ModuleFactory
+import mageaddons.events.TabListEvent
 import mageaddons.utils.Color
 import mageaddons.utils.PlayerUtils.mcText
+import mageaddons.utils.TabList
 import mageaddons.utils.impl.Blessing
+import mageaddons.utils.noControlCodes
+import mageaddons.utils.romanToInt
+import net.minecraft.network.play.server.S47PacketPlayerListHeaderFooter
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 object BlessingDisplay: ModuleFactory(
     name = "Blessing Display",
     toggle = Config.blessingDisplay
 ) {
+    var blessingDis: List<Int> = listOf()
     private val power = Config.displayPower
     private val time = Config.displayTime
     private val stone = Config.displayStone
@@ -23,5 +30,16 @@ object BlessingDisplay: ModuleFactory(
         BlessingData(Blessing.LIFE, { life }, { Color.RED }),
         BlessingData(Blessing.WISDOM, { wisdom }, { Color.BLUE })
     )
+    @SubscribeEvent
+    fun onHeaderPacket(event: TabListEvent) {
+        if (event.packet is S47PacketPlayerListHeaderFooter) {
+            handleHeaderFooterPacket(event.packet)
+        }
+    }
 
+    private fun handleHeaderFooterPacket(packet: S47PacketPlayerListHeaderFooter) {
+        Blessing.entries.forEach { blessing ->
+            blessing.regex.find(packet.footer.unformattedText.noControlCodes)?.let { match -> blessing.current = romanToInt(match.groupValues[1]) }
+        }
+    }
 }
--- src/main/kotlin/mageaddons/utils/Utils.kt
+++ src/main/kotlin/mageaddons/utils/Utils.kt
@@ -35,6 +35,21 @@ private val FORMATTING_CODE_PATTERN = Regex("§[0-9a-fk-or]", RegexOption.IGNORE
 val String?.noControlCodes: String
     get() = this?.let { FORMATTING_CODE_PATTERN.replace(it, "") } ?: ""
 
+private val romanMap = mapOf('I' to 1, 'V' to 5, 'X' to 10, 'L' to 50, 'C' to 100, 'D' to 500, 'M' to 1000)
+private val numberRegex = Regex("^[0-9]+$")
+fun romanToInt(s: String): Int {
+    return if (s.matches(numberRegex)) s.toInt()
+    else {
+        var result = 0
+        for (i in 0 until s.length - 1) {
+            val current = romanMap[s[i]] ?: 0
+            val next = romanMap[s[i + 1]] ?: 0
+            result += if (current < next) -current else current
+        }
+        result + (romanMap[s.last()] ?: 0)
+    }
+}
+
 /**
  * Checks if the current object is equal to at least one of the specified objects.
  *
